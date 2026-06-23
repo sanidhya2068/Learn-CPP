d372dc892ffe4f5370fd773148d853370812c341
SANIDHYA  SINGH RATHORE <sanidhyasinghrathore5@gmail.com>
2026-06-23 22:01:33 +0530

    Implement simple sum program in C++

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a , b;
+
+    cout<<"enter first no.: ";
+    cin>>a;
+
+    cout<<"enter secound no.: ";
+    cin>>b;
+
+     int sum = a+b;
+     cout<<"sum = " <<sum <<endl;
+    return 0;
+}
