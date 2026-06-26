aadd1d9c82c775ff8597bda684d647c592a4812d
SANIDHYA  SINGH RATHORE <sanidhyasinghrathore5@gmail.com>
2026-06-26 13:43:02 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -14,3 +14,20 @@ int main() {
      cout<<"sum = " <<sum <<endl;
     return 0;
 }
+
+CALCULATE TOTAL SUM THROUGH WHILE LOOP.
+int main(){
+    int n;
+    cout<<"Enter the no.: ";
+    cin>>n;
+
+    int i=1;
+    int sum = 0;
+    while(i<=n){
+        sum+=i;
+        i+=1;
+    }
+
+    cout<<"Total sum is: " <<sum;
+    return 0;
+}
