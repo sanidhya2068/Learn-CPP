458be8c08bc7d1a5f343ba7e83620de3a5ad9d67
SANIDHYA  SINGH RATHORE <sanidhyasinghrathore5@gmail.com>
2026-07-01 15:43:58 +0530

    Create 01_Single_Number.cpp

---
+++ 02-DSA/01-Arrays/questions_of_vector/01_Single_Number.cpp
@@ -0,0 +1,18 @@
+QUESTIONS OF VECTOR-->
+
+1) SINGLE NUMBER 
+
+int singleNumber(){
+   vector<int>nums = {1,2,3,4,3,2,1};
+   int ans = 0;
+   for(int num:nums){
+    ans^=num;
+
+   }
+return ans;
+}
+int main(){
+    cout<<singleNumber()<<endl;
+    
+return 0;
+}
